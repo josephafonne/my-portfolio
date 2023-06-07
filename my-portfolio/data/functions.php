c3bf9445cb0312fa8afb9838058dbf63d600c2e9
josephafonne <72078016+josephafonne@users.noreply.github.com>
2023-06-07 22:46:33 +0200

    Add files via upload

---
+++ my-portfolio/data/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+require("conn.php");
+
+function getSkills(){
+    global $conn;
+    $query = "SELECT * FROM skills";
+    $sql = mysqli_query($conn, $query);
+
+    if($sql){
+       foreach ($sql as $row) {
+        echo "<h1>" . $row['name'] ."</h1> <p>". $row['description'] ."</p><br>";
+       }
+    } else {
+        //Failure
+    }
+}
+
+function getProjects(){
+    global $conn;
+    $query = "SELECT * FROM projects";
+    $sql = mysqli_query($conn, $query);
+
+    if($sql){
+       foreach ($sql as $row) {
+        echo '<div class="card">
+        <img src="assets/images/' . $row["image-link"] .'" alt="Avatar" style="width:100%">
+        <div class="container">
+            <h4><b>' . $row["name"] .'</b></h4>
+            <p>' . substr($row["description"], 0, 100) . "..." .'</p>
+        </div>
+    </div>';
+       }
+    } else {
+        //Failure
+    }
+}
+
+?>
---
+++ my-portfolio/index.php
@@ -0,0 +1,39 @@
+<?php $page = "home"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS styles -->
+    <link rel="stylesheet" href="assets/css/styles.css" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/icons/icon.png" />
+    <title>Joseph Menye Afonne | Developer</title>
+</head>
+<body>
+<?php include('assets/includes/navbar.php') ?>
+    <!-- Main Section -->
+    <div class="hero">
+        <div class="hero-text">
+            <h1 class="splash">Joseph Menye</h1><br>
+            <p>Full Stack Developer & Programmer</p><br>
+            <a href="skills.html"><button>See more</button><br></a>
+        </div>
+    </div>
+    
+    <!-- Footer Section -->
+    <footer>
+        <p class="footer-text">&copy; Menye 2023.</p>
+    </footer>   
+
+    <!-- Website scripts -->
+    <script src="assets/js/app.js"></script>
+
+    <!-- Icons scripts -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
